6cff0fe021ec1c9afae7db55c06f56591eefadf8
Victor Manuel Aguado Alvarez <vaguado@ia.com.mx>
2018-12-14 13:53:23 -0600

    First Commit

---
+++ android/app/src/main/kotlin/mx/com/ia/enterprise/prueba/myexamplesapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package mx.com.ia.enterprise.prueba.myexamplesapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
